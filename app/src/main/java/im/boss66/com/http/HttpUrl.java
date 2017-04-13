9a510440bbcc9fc27ed40f78a8155dedd16c9629
duanjisi <576248427@qq.com>
2017-04-13 17:42:35 +0800

    修复学校名人详情页跳转的bug

--- app/src/main/java/im/boss66/com/http/HttpUrl.java
+++ app/src/main/java/im/boss66/com/http/HttpUrl.java
@@ -248,8 +248,10 @@ public class HttpUrl {
     public static final String FUWA_PAY_ALIPAY = MAIN_URL + "api/pay/alipay";
     //支付系统通知购买成功
     public static final String SYSTEM_PAY_NOTICE = "http://wsimali.66boss.com:9090/msg/notice?";
-    //名人详情页
+    //家乡名人详情页
     public static final String FAMOUSPERSON_DETAIL = MAIN_URL + "web/hometown/celebrity";
+    //学校名人页
+    public static final String SCHOOL_PERSON_DETAIL =MAIN_URL+"web/school/celebrity";
     //学校社团详情页
     public static final String SCHOOL_CLUB_DETATIL = MAIN_URL + "web/school/league";
     //家乡动态详情页
