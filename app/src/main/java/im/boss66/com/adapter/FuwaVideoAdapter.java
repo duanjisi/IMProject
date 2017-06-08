4d7a2c597ec7bbf19ec24e16f9991fe16cb0be6f
duanjisi <576248427@qq.com>
2017-06-08 16:00:03 +0800

    加上跳转部落功能

--- app/src/main/java/im/boss66/com/adapter/FuwaVideoAdapter.java
+++ app/src/main/java/im/boss66/com/adapter/FuwaVideoAdapter.java
@@ -1,6 +1,7 @@
 package im.boss66.com.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
@@ -11,13 +12,27 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.JSON;
 import com.bumptech.glide.Glide;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
+import java.util.List;
+
+import im.boss66.com.App;
+import im.boss66.com.Constants;
 import im.boss66.com.R;
 import im.boss66.com.Utils.ImageLoaderUtils;
+import im.boss66.com.Utils.ToastUtil;
 import im.boss66.com.Utils.UIUtils;
+import im.boss66.com.activity.connection.ClanClubActivity;
 import im.boss66.com.entity.FuwaVideoEntity;
+import im.boss66.com.entity.TribeEntity;
+import im.boss66.com.http.HttpUrl;
 
 /**
  * Created by liw on 2017/6/1.
@@ -38,13 +53,15 @@ public class FuwaVideoAdapter extends BaseRecycleViewAdapter {
     public void onBindItemHolder(RecyclerView.ViewHolder holder, final int position) {
         FuwaVideoHolder holder1 = (FuwaVideoHolder) holder;
 
-        FuwaVideoEntity.DataBean item = (FuwaVideoEntity.DataBean) datas.get(position);
+        final FuwaVideoEntity.DataBean item = (FuwaVideoEntity.DataBean) datas.get(position);
 
 
         String height = item.getHeight();
         String width = item.getWidth();
         Log.i("liwya",width+"实际宽度");
         int screenWidth = UIUtils.getScreenWidth(context)/2;
+        int padding = UIUtils.dip2px(context, 6);
+        screenWidth = screenWidth-padding;
         Log.i("liwya",screenWidth+"屏幕宽度");
 
         double v = Double.parseDouble(width)/screenWidth;
@@ -93,6 +110,67 @@ public class FuwaVideoAdapter extends BaseRecycleViewAdapter {
                 itemListener.onItemClick(position);
             }
         });
+        holder1.img_head.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initTribe(item.getUserid());
+            }
+        });
+    }
+
+
+    private void initTribe(String creatorid) {
+
+        String url = HttpUrl.SEARCH_TRIBE_LIST;
+        HttpUtils httpUtils = new HttpUtils(60 * 1000);//实例化RequestParams对象
+        com.lidroid.xutils.http.RequestParams params = new com.lidroid.xutils.http.RequestParams();
+        params.addBodyParameter("access_token", App.getInstance().getAccount().getAccess_token());
+        url = url + "?user_id=" + creatorid;
+
+        httpUtils.send(HttpRequest.HttpMethod.POST, url, params, new RequestCallBack<String>() {
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                String result = responseInfo.result;
+                if (result != null) {
+                    TribeEntity tribeEntity = JSON.parseObject(result, TribeEntity.class);
+                    int code = tribeEntity.getCode();
+                    if (code == 1) {
+                        List<TribeEntity.ResultBean> beans = tribeEntity.getResult();
+                        TribeEntity.ResultBean bean = beans.get(0);
+                        String name = bean.getName();
+                        int stribe_id = bean.getStribe_id();
+                        int user_id = bean.getUser_id();
+
+                        Intent intent = new Intent(context, ClanClubActivity.class);
+                        intent.putExtra("isClan", 3);
+                        intent.putExtra("name", name);
+                        intent.putExtra("id", stribe_id+"");
+                        intent.putExtra("user_id", user_id+"");
+                        context.startActivity(intent);
+
+                    } else {
+                        //code==0 没数据，没部落 不作处理
+                    }
+
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(HttpException e, String s) {
+                int code = e.getExceptionCode();
+                if (code == 401) {
+                    Intent intent = new Intent();
+                    intent.setAction(Constants.ACTION_LOGOUT_RESETING);
+                    App.getInstance().sendBroadcast(intent);
+                } else {
+                    ToastUtil.showShort(context,e.getMessage());
+                }
+            }
+        });
+
+
     }
 
     @Override
--- app/src/main/java/im/boss66/com/fragment/VideoListFragment.java
+++ app/src/main/java/im/boss66/com/fragment/VideoListFragment.java
@@ -108,7 +108,7 @@ public class VideoListFragment extends BaseFragment {
 
 
         String url = HttpUrl.SEARCH_VIDEO_LIST + "?geohash=" + lng + "-" + lat + "&class=" + classid;
-
+//        Log.i("liwya",url);
         HttpUtils httpUtils = new HttpUtils(60 * 1000);//实例化RequestParams对象
         httpUtils.send(HttpRequest.HttpMethod.GET, url, new RequestCallBack<String>() {
 
@@ -116,6 +116,7 @@ public class VideoListFragment extends BaseFragment {
             public void onSuccess(ResponseInfo<String> responseInfo) {
                 result = responseInfo.result;
 
+//                Log.i("liwya",result);
                 if (result != null) {
                     FuwaVideoEntity fuwaVideoEntity = JSON.parseObject(result, FuwaVideoEntity.class);
                     if (fuwaVideoEntity.getCode() == 0) {
